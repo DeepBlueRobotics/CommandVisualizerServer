a6e50e369ab71c22540b599be8127024fdd11fc7
CoolSpy3 <coolspythree@gmail.com>
2023-05-04 20:12:36 -0700

    store non-running command

--- src/main/java/org/carlmontrobotics/commandvisualizer/CommandVisualizer.java
+++ src/main/java/org/carlmontrobotics/commandvisualizer/CommandVisualizer.java
@@ -1,8 +1,11 @@
 package org.carlmontrobotics.commandvisualizer;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.WeakHashMap;
 import java.util.function.Consumer;
 
 import org.carlmontrobotics.lib199.Lib199Subsystem;
@@ -18,12 +21,14 @@ public class CommandVisualizer {
 
     public static final String NT_KEY = "CommandDescriptors";
 
-    private static HashSet<Consumer<String>> loggers = new HashSet<>();
-    private static HashSet<Command> runningCommands = new HashSet<>();
+    private static Set<Consumer<String>> loggers = new HashSet<>();
+    private static Set<Command> runningCommands = new HashSet<>();
+    private static Set<Command> allCommands = Collections.newSetFromMap(new WeakHashMap<>());
     private static boolean disabled = false;
 
     static {
         Lib199Subsystem.registerPeriodic(CommandVisualizer::logCommands);
+        CommandScheduler.getInstance().onCommandInitialize(allCommands::add);
         CommandScheduler.getInstance().onCommandInitialize(runningCommands::add);
         CommandScheduler.getInstance().onCommandFinish(runningCommands::remove);
         CommandScheduler.getInstance().onCommandInterrupt(runningCommands::remove);
@@ -43,7 +48,7 @@ public class CommandVisualizer {
             return;
         String descriptorJson;
         try {
-            descriptorJson = new ObjectMapper().writeValueAsString(getProcessedRunningCommands());
+            descriptorJson = new ObjectMapper().writeValueAsString(getProcessedCommands());
         } catch (Exception e) {
             e.printStackTrace();
             return;
@@ -61,14 +66,18 @@ public class CommandVisualizer {
         disabled = false;
     }
 
+    public static ArrayList<Command> getAllCommands() {
+        return new ArrayList<>(allCommands);
+    }
+
     public static ArrayList<Command> getRunningCommands() {
         return new ArrayList<>(runningCommands);
     }
 
-    public static CommandDescriptor[] getProcessedRunningCommands() {
-        return runningCommands.stream().map(command -> {
+    public static CommandDescriptor[] getProcessedCommands() {
+        return allCommands.stream().map(command -> {
             try {
-                return CommandDescriptorFactory.fromCommand(command, true);
+                return CommandDescriptorFactory.fromCommand(command, runningCommands.contains(command));
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
